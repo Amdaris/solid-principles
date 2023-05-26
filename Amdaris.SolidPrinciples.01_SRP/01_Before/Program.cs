ebd0a99c333031109afc9b6ffbfe602bf8f0934b
Kiril Domuschiev <kiril.domuschiev@amdaris.com>
2023-05-26 11:41:18 +0300

    SRP before and after implementations.

--- Amdaris.SolidPrinciples.01_SRP/01_Before/Program.cs
+++ Amdaris.SolidPrinciples.01_SRP/01_Before/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Logger logger = new Logger();
+
+logger.LogMessage("Cool message which should be logged!");
+
+logger.SendNotification("Amazing notification has been sended!");
+
+#region Classes
+public class Logger
+{
+    public void LogMessage(string message)
+    {
+        Console.WriteLine($"Log message: {message}");
+    }
+
+    public void SendNotification(string notificationMessage)
+    {
+        Console.WriteLine($"Notification message: {notificationMessage}");
+    }
+}
+#endregion
--- Amdaris.SolidPrinciples.01_SRP/02_After/Program.cs
+++ Amdaris.SolidPrinciples.01_SRP/02_After/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LoggerService loggerService = new LoggerService();
+
+loggerService.LogMessage("Cool message which should be logged!");
+
+NotificationService notificationService = new NotificationService();
+
+notificationService.SendNotification("Amazing notification has been sended!");
+
+#region Classes
+public class LoggerService
+{
+    public void LogMessage(string message)
+    {
+        Console.WriteLine($"Log message: {message}");
+    }
+}
+
+public class NotificationService
+{
+    public void SendNotification(string notificationMessage)
+    {
+        Console.WriteLine($"Notification message: {notificationMessage}");
+    }
+}
+#endregion
