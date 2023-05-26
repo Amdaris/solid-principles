ab94bf6bab5e71d7e8ef7f175b82ff080d09faf6
Kiril Domuschiev <kiril.domuschiev@amdaris.com>
2023-05-26 15:47:04 +0300

    OCP before and after implementation.

--- Amdaris.SolidPrinciples.02_OCP/01_before/Program.cs
+++ Amdaris.SolidPrinciples.02_OCP/01_before/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Payment creditCardPayment = new Payment(PaymentType.CreditCard);
+Payment paypalPayment = new Payment(PaymentType.PayPal);
+
+PaymentProcessor paymentProcessor = new PaymentProcessor();
+
+paymentProcessor.ProcessPayment(creditCardPayment);
+paymentProcessor.ProcessPayment(paypalPayment);
+
+#region Classes
+public enum PaymentType
+{
+    CreditCard,
+    PayPal
+}
+
+public class Payment
+{
+    public Payment(PaymentType type)
+    {
+        Type = type;
+    }
+
+    public PaymentType Type { get; set; }
+    public decimal Amount { get; set; }
+}
+
+public class PaymentProcessor
+{
+    public void ProcessPayment(Payment payment)
+    {
+        if (payment.Type == PaymentType.CreditCard)
+        {
+            Console.WriteLine("Processing credit card payment ...");
+        }
+        else if (payment.Type == PaymentType.PayPal)
+        {
+            Console.WriteLine("Processing paypal payment ...");
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid payment type.");
+        }
+    }
+}
+#endregion
--- Amdaris.SolidPrinciples.02_OCP/02_after/Program.cs
+++ Amdaris.SolidPrinciples.02_OCP/02_after/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Payment creditCardPayment = new Payment(PaymentType.CreditCard);
+Payment paypalPayment = new Payment(PaymentType.PayPal);
+
+PaymentProcessor paymentProcessor = new PaymentProcessor();
+
+paymentProcessor.AddPaymentProcessor(PaymentType.CreditCard, new CreditCardPaymentProcessor());
+paymentProcessor.AddPaymentProcessor(PaymentType.PayPal, new PayPalPaymentProcessor());
+
+paymentProcessor.ProcessPayment(creditCardPayment);
+paymentProcessor.ProcessPayment(paypalPayment);
+
+#region Classes 
+public enum PaymentType
+{
+    CreditCard,
+    PayPal
+}
+
+public class Payment
+{
+    public Payment(PaymentType type)
+    {
+        Type = type;
+    }
+
+    public PaymentType Type { get; set; }
+    public decimal Amount { get; set; }
+}
+
+public interface IPaymentProcessor
+{
+    void ProcessPayment(Payment payment);
+}
+
+public class CreditCardPaymentProcessor : IPaymentProcessor
+{
+    public void ProcessPayment(Payment payment)
+    {
+        Console.WriteLine("Processing credit card payment ...");
+    }
+}
+
+public class PayPalPaymentProcessor : IPaymentProcessor
+{
+    public void ProcessPayment(Payment payment)
+    {
+        Console.WriteLine("Processing paypal payment ...");
+    }
+}
+
+public class PaymentProcessor
+{
+    private readonly Dictionary<PaymentType, IPaymentProcessor> paymentProcessors;
+
+    public PaymentProcessor()
+    {
+        paymentProcessors = new Dictionary<PaymentType, IPaymentProcessor>();
+    }
+
+    public void AddPaymentProcessor(PaymentType paymentType, IPaymentProcessor paymentProcessor)
+    {
+        paymentProcessors.Add(paymentType, paymentProcessor);
+    }
+
+    public void ProcessPayment(Payment payment)
+    {
+        if (paymentProcessors.ContainsKey(payment.Type))
+        {
+            IPaymentProcessor paymentProcessor = paymentProcessors[payment.Type];
+            paymentProcessor.ProcessPayment(payment);
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid payment type.");
+        }
+    }
+}
+#endregion
