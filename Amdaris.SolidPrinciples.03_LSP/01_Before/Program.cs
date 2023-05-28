4da4efdfd9a708ed544905e908d152f9eb74166c
Kiril Domuschiev <kiril.domuschiev@amdaris.com>
2023-05-28 10:17:28 +0300

    LSP before and after implementations.

--- Amdaris.SolidPrinciples.03_LSP/01_Before/Program.cs
+++ Amdaris.SolidPrinciples.03_LSP/01_Before/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Rectangle rectangle = new Square();
+
+rectangle.Width = 5;
+rectangle.Height = 10;
+
+Console.WriteLine($"Rectangle area: {rectangle.CalculateArea()}");
+
+#region Classes
+public class Rectangle
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+
+    public int CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Square : Rectangle
+{
+    public override int Width
+    {
+        get { return base.Width; } 
+        set
+        {
+            base.Width = value;
+            base.Height = value;
+        }
+    }
+
+    public override int Height
+    {
+        get { return base.Height; }
+        set
+        {
+            base.Width = value;
+            base.Height = value;
+        }
+    }
+}
+#endregion
--- Amdaris.SolidPrinciples.03_LSP/02_After/Program.cs
+++ Amdaris.SolidPrinciples.03_LSP/02_After/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Shape rectangle = new Rectangle() { Width = 5, Height = 10 };
+Console.WriteLine($"Rectangle area: {rectangle.CalculateArea()}");
+
+Shape square = new Square() { SideLength = 5 };
+Console.WriteLine($"Square area: {square.CalculateArea()}");
+
+#region Classes
+public class Shape
+{
+    public virtual int CalculateArea()
+    {
+        return 0;
+    }
+}
+
+public class Rectangle : Shape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public override int CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Square : Shape
+{
+    public int SideLength { get; set; }
+
+    public override int CalculateArea()
+    {
+        return SideLength * SideLength;
+    }
+}
+#endregion
