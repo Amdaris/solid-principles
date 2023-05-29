11ff3c2d1d0ac45561e6de99cb00cd7552ec25e2
Kiril Domuschiev <kiril.domuschiev@amdaris.com>
2023-05-29 14:43:34 +0300

    DIP before and after implementations.

--- Amdaris.SolidPrinciples.05_DIP/01_Before/Program.cs
+++ Amdaris.SolidPrinciples.05_DIP/01_Before/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿NotificationService notificationService = new NotificationService();
+
+notificationService.SendNotification("Amdaris", "SOLID principles are amazing!");
+
+#region Classes
+public class EmailSender
+{
+    public void SendEmail(string recipient, string message)
+    {
+        Console.WriteLine($"An email was sent to {recipient}! Message: {message}");
+    }
+}
+
+public class NotificationService
+{
+    private EmailSender _emailSender;
+
+    public NotificationService()
+    {
+        _emailSender = new EmailSender();
+    }
+
+    public void SendNotification(string recipient, string message)
+    {
+        _emailSender.SendEmail(recipient, message);
+    }
+}
+#endregion
--- Amdaris.SolidPrinciples.05_DIP/02_After/Program.cs
+++ Amdaris.SolidPrinciples.05_DIP/02_After/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿NotificationService notificationService = new NotificationService(new EmailSender());
+
+notificationService.SendNotification("Amdaris", "SOLID principles are amazing!");
+
+#region Classes
+public interface INotificationSender
+{
+    void SendNotification(string recipient, string message);
+}
+
+public class EmailSender : INotificationSender
+{
+    public void SendNotification(string recipient, string message)
+    {
+        Console.WriteLine($"An email was sent to {recipient}! Message: {message}");
+    }
+}
+
+public class NotificationService
+{
+    private INotificationSender _notificationSender;
+
+    public NotificationService(INotificationSender notificationSender)
+    {
+        _notificationSender = notificationSender;
+    }
+
+    public void SendNotification(string recipient, string message)
+    {
+        _notificationSender.SendNotification(recipient, message);
+    }
+}
+#endregion
