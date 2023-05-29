0d8d4f549c66c843564478522f5acc47b2fb2717
Kiril Domuschiev <kiril.domuschiev@amdaris.com>
2023-05-29 10:49:40 +0300

    ISP before and after implementations.

--- Amdaris.SolidPrinciples.04_ISP/01_Before/Program.cs
+++ Amdaris.SolidPrinciples.04_ISP/01_Before/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Classes
+public interface IAnimal
+{
+    void Eat();
+    void Sleep();
+    void Fly();
+    void Swim();
+}
+
+public class Bird : IAnimal
+{
+    public void Eat()
+    {
+        // Code for eating
+    }
+
+    public void Sleep()
+    {
+        // Code for sleeping
+    }
+
+    public void Fly()
+    {
+        // Code for flying
+    }
+
+    public void Swim()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Fish : IAnimal
+{
+    public void Eat()
+    {
+        // Code for eating
+    }
+
+    public void Sleep()
+    {
+        // Code for sleeping
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Swim()
+    {
+        // Code for swimming
+    }
+}
+#endregion
--- Amdaris.SolidPrinciples.04_ISP/02_After/Program.cs
+++ Amdaris.SolidPrinciples.04_ISP/02_After/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Classes
+public interface IAnimal
+{
+    void Eat();
+    void Sleep();
+}
+
+public interface IFlyable
+{
+    void Fly();
+}
+
+public interface ISwimmable
+{
+    void Swim();
+}
+
+public class Bird : IAnimal, IFlyable
+{
+    public void Eat()
+    {
+        // Code for eating
+    }
+
+    public void Sleep()
+    {
+        // Code for sleeping
+    }
+
+    public void Fly()
+    {
+        // Code for flying
+    }
+}
+
+public class Fish : IAnimal, ISwimmable
+{
+    public void Eat()
+    {
+        // Code for eating
+    }
+
+    public void Sleep()
+    {
+        // Code for sleeping
+    }
+
+    public void Swim()
+    {
+        // Code for swimming
+    }
+}
+#endregion
